bb96a8f2dd5e8972dce231221ece75c9633077e3
shashidarette <se146@student.le.ac.uk>
2018-04-08 12:13:08 +0000

    RoadStatus test project

---
+++ RoadStatus.Library.Tests/RoadStatusTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoadApi.Library.Tests
+{
+    /**
+     * This test class details the tests for the requirements of RoadStatus client.
+     * The scope of the client is to check the validity of road based on the id.
+     * In addition it should also retrieve basic informaiton such as Display Name (different from RoadID),
+     * Status and StatusDescription.
+     * */
+    [TestClass]
+    public class RoadStatusTests
+    {
+        //Given a valid road ID is specified
+        //When the client is run
+        //Then the road ‘displayName’ should be displayed
+
+        //Given a valid road ID is specified
+        //When the client is run
+        //Then the road ‘statusSeverity’ should be displayed as ‘Road Status’
+
+        //Given a valid road ID is specified
+        //When the client is run
+        //Then the road ‘statusSeverityDescription’ should be displayed as ‘Road Status Description’
+
+        //Given an invalid road ID is specified
+        //When the client is run
+        //Then the application should return an informative error
+
+        //Given an invalid road ID is specified
+        //When the client is run
+        //Then the application should exit with a non-zero System Error code
+
+        [TestMethod]
+        public void BasicTest()
+        {
+        }
+    }
+}
