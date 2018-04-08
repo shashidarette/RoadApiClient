8c183e7ee9e5aa63d2f7dcf759b291f1904f3f3b
shashidarette <se146@student.le.ac.uk>
2018-04-08 14:32:05 +0000

    RoadStatus - GetStatus - implementation

--- RoadApi.Library/RoadStatus.cs
+++ RoadApi.Library/RoadStatus.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Tfl.Api.Presentation.Entities;
 
 namespace RoadApi.Library
 {
@@ -33,21 +38,37 @@ namespace RoadApi.Library
             RoadInformation info = new RoadInformation();
             if (ValidateApiKeys())
             {
-                if (roadId != "A1")
-                {
-                    info.Valid = false;
-                    LastErrorCode = "404";
-                    LastErrorMessage = "Road does not exist.";
-                    return info;
-                }
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                HttpClient apiClient = new HttpClient();
+                apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                apiClient.BaseAddress = new Uri(TfLRoadUrl);
 
-                info.Valid = true;
-                info.Name = roadId;
-                info.StatusSeverity = "Good";
-                info.StatusSeverityDescription = "No delays";
-            } else
+                HttpResponseMessage response = apiClient.GetAsync("Road/" + roadId + TflKeys).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    var roadStatuses = JsonConvert.DeserializeObject<RoadCorridor[]>(responseString);
+
+                    // consider only one road
+                    if (roadStatuses.Length > 0)
+                    {
+                        info.Valid = true;
+                        info.Name = roadStatuses[0].DisplayName;
+                        info.StatusSeverity = roadStatuses[0].StatusSeverity;
+                        info.StatusSeverityDescription = roadStatuses[0].StatusSeverityDescription;
+                    }
+                }
+                else
+                {
+                    LastErrorCode = response.StatusCode;
+                    LastErrorMessage = response.ReasonPhrase;
+                    info.Valid = false;
+                }
+            }
+            else
             {
-                throw new InvalidOperationException("TfL Api keys have not been submitted.");
+                throw new InvalidOperationException("TfL Api keys have not been initialized.");
             }
             return info;
         }
